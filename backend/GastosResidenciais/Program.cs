3c077896efb1099b35615fd7a4c897d81aad4e40
josephyzz <miguelneto275@gmail.com>
2026-07-07 16:49:35 -0300

    fix: resolved Cors error

--- backend/GastosResidenciais/Program.cs
+++ backend/GastosResidenciais/Program.cs
@@ -9,7 +9,24 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<DataContext>();
 
+// Liberando acesso de cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "AllowAll",
+        policy =>
+        {
+            policy
+                .AllowAnyOrigin() // Permite qualquer origem
+                .AllowAnyMethod() // Permite GET, POST, PUT, DELETE, etc.
+                .AllowAnyHeader(); // Permite qualquer cabeçalho
+        }
+    );
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 if (app.Environment.IsDevelopment())
 {
